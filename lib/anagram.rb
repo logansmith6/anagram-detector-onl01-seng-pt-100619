e835fa6a28501de537d74e39ecf3b5958cc4ff5d
Logan Smith <logsmith06@gmail.com>
2019-11-24 22:32:48 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,15 +3,29 @@ require 'pry'
 class Anagram
 
   attr_accessor :word
-
+  @@winners = []
   def initialize(word)
     @word = word
   end
 
   def match(possible_anagrams)
 
-     temp = @word.split("")
+     temp1 = @word.split("")
+
+     possible_anagrams.each do |winner|
+       temp2 = winner.split("")
+
+       if temp2.sort == temp1.sort
+         @@winners << temp2 
+       end
+
+       @@winners
+         
+     end
+     
     binding.pry
+
+    
   end
 
 end
