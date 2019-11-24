345d41908fc51b2938194254a820a93c4835e55f
Logan Smith <logsmith06@gmail.com>
2019-11-24 22:25:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,16 @@
 # Your code goes here!
+require 'pry'
 class Anagram
 
   attr_accessor :word
 
+  def initialize(word)
+    @word = word 
+  end
 
+  def match(possible_anagrams)
+    temp = @word.split(" ")
+    binding.pry 
+  end 
+  
 end
